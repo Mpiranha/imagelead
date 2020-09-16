a8a732a194611cc0c88a3a88ca66d382ba1322d3
mpiranha <chukwubuezeokeke@gmail.com>
2020-09-16 21:17:06 +0100

    update

--- js/script.js
+++ js/script.js
@@ -13,4 +13,36 @@
             $('.more-option').removeClass('show');
         }
     });
+
+    $('.btn-add-pixel').on('click', function () {
+        $('.shade').addClass('show-action');
+    });
+
+    $('.visibility-icon').on('click', function () {
+        $('.shade').addClass('show-action');
+    });
+
+    $('.btn-cancel').on('click', function () {
+        $('.shade').removeClass('show-action');
+    });
+
+    $('.edit-banner').on('click', function () {
+        $('.shade').removeClass('show-action');
+        $('.shade').addClass('show-action-edit');
+    });
+    $('.btn-cancel-sm').on('click', function () {
+        $('.shade').removeClass('show-action-edit');
+        $('.shade').addClass('show-action');
+    });
+
+    $('#cancel-publish').on('click', function () {
+        $('.shade').removeClass('show-action-publish');
+        //$('.shade').addClass('show-action');
+    });
+
+    $('#cancel-share').on('click', function () {
+        $('.shade').removeClass('show-action-share');
+        //$('.shade').addClass('show-action');
+    });
+
 })();
